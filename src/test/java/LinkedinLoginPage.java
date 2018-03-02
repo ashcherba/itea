e4a77f1a980c88e4a220833d8261864d33f4761b
anna <shcherba.anya@gmail.com>
2018-03-02 10:04:34 +0200

    LoginPage - tutors samle

--- src/test/java/LinkedinLoginPage.java
+++ src/test/java/LinkedinLoginPage.java
@@ -1,39 +1,33 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LinkedinLoginPage {
+public class LinkedinLoginPage extends LinkedinBasePage{
     WebDriver driver;
-    public LinkedinLoginPage (WebDriver driver) {
+
+    public LinkedinLoginPage (WebDriver driver){
+        super(driver);
         this.driver = driver;
     }
+
     private WebElement emailField;
-    private WebElement passField;
+    private WebElement passwordField;
     private WebElement signInButton;
 
-    public void initElements() {
-        emailField = driver.findElement(By.xpath("//input[(@id='login-email')]"));
-        waitUntilElementsClickable(emailField,5);
-        passField = driver.findElement(By.xpath("//input[(@id='login-password')]"));
+    private void initElements(){
+        emailField = driver.findElement(By.id("login-email"));
+        waitUntilElementIsClickable(emailField, 5);
+        passwordField = driver.findElement(By.id("login-password"));
         signInButton = driver.findElement(By.id("login-submit"));
     }
 
-    public void loginAs(String email, String password){
+    public LinkedinBasePage loginAs(String email, String password){
         initElements();
         emailField.sendKeys(email);
-        passField.sendKeys(password);
+        passwordField.sendKeys(password);
         signInButton.click();
+        return new LinkedinBasePage(driver);
     }
 
-    public void waitUntilElementsClickable(WebElement WebElement){
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(ExpectedConditions.elementToBeClickable(WebElement));
-    }
 
-    public void waitUntilElementsClickable(WebElement WebElement, int timeOutInSecond){
-        WebDriverWait wait = new WebDriverWait(driver, timeOutInSecond);
-        wait.until(ExpectedConditions.elementToBeClickable(WebElement));
-    }
 }
