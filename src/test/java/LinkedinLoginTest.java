62676759c667c02c2dff9d920b68aa22b7f8792d
anna <shcherba.anya@gmail.com>
2018-03-02 10:05:15 +0200

    LoginTest - tutors samle

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -5,92 +5,55 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-import static java.lang.Thread.sleep;
-
 public class LinkedinLoginTest {
     WebDriver driver;
 
-    @BeforeClass
-    public void beforeClass() {
+    @BeforeMethod
+    public void beforeTest(){
+        driver = new FirefoxDriver();
+        driver.get("https://www.linkedin.com/");
     }
 
-    @AfterClass
-    public void afterClass () {
-    }
-    @BeforeMethod
-    public void beforeTest() {
-        driver = new FirefoxDriver();
-        driver.get("https://www.linkedin.com");
-    }
     @AfterMethod
     public void afterTest() {
         driver.close();
-
     }
-
 
     @Test
-    public void successfulLoginTest() throws InterruptedException {
-        driver.get("https://www.linkedin.com");
-        String initialGetTitle = driver.getTitle();
-        String initialPageUrl = driver.getCurrentUrl();
+    public void successfulLoginTest() {
+        LinkedinLoginPage loginPage = new LinkedinLoginPage(driver);
 
-        WebElement emailField = driver.findElement(By.xpath("//input[(@id='login-email')]"));
-        WebElement passField = driver.findElement(By.xpath("//input[(@id='login-password')]"));
-        WebElement signInButton = driver.findElement(By.id("login-submit"));
+        String initialPageTitle = loginPage.getPageTitle();
+        String initialPageUrl = loginPage.getPageUrl();
 
-        emailField.sendKeys("aashcherba@bigmir.net\n");
-        passField.sendKeys("qwertyQ1");
-        signInButton.click();
+        Assert.assertEquals(initialPageTitle, "LinkedIn: Log In or Sign Up",
+                "Login page title is wrong");
 
-        WebElement userIcon = driver.findElement(By.id("profile-nav-item"));
-        Assert.assertTrue(userIcon.isDisplayed(), "User icon was not displayed");
-        Assert.assertNotEquals(driver.getTitle(),initialGetTitle, "Page title didn't change");
-        Assert.assertNotEquals(driver.getCurrentUrl(),initialPageUrl, "Page URL didn't change");
-        Assert.assertEquals(driver.getTitle(), "Крупнейшая в мире сеть профессиональных контактов | LinkedIn", "Login page Title is wrong");
+        LinkedinBasePage homePage = loginPage.loginAs("iteatest@i.ua", "1q2w3e_4r5t");
+        Assert.assertTrue(homePage.isSignedIn(), "User is not signed in");
 
-
-//my HW:
-        /*sleep(5000);
-        String currentLink = driver.getCurrentUrl();
-        Assert.assertEquals(currentLink, "https://www.linkedin.com/feed/");
-
-        WebElement homePage = driver.findElement(By.xpath("//a[(@class='nav-item__link nav-item__link--underline js-nav-item-link active')]"));
-        Assert.assertTrue(homePage.isEnabled(), "Invalid credentials");
-        driver.quit();
-        */
+        Assert.assertNotEquals(homePage.getPageTitle(), initialPageTitle,
+                "Page title did not change after login");
+        Assert.assertNotEquals(homePage.getPageUrl(), initialPageUrl,
+                "Page url did not change after login");
     }
 
-/*    @Test
-    public void successfulLoginTest2() throws InterruptedException {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.linkedin.com/");
-        WebElement emailField = driver.findElement(By.xpath("//input[(@id='login-email')]"));
-        WebElement passField = driver.findElement(By.xpath("//input[(@id='login-password')]"));
-        WebElement signInButton = driver.findElement(By.id("login-submit"));
-
-        emailField.sendKeys("aashcherba@bigmir.net\n");
-        passField.sendKeys("qwertyQ1");
-        signInButton.click();
-
-        sleep(5000);
-        String currentLink = driver.getCurrentUrl();
-        Assert.assertEquals(currentLink, "https://www.linkedin.com/feed/");
-        driver.quit();
-    }
-*/
     @Test
-    public void negativeLoginTest(){
-        driver.get("https://www.linkedin.com/");
-        WebElement emailField = driver.findElement(By.xpath("//input[(@id='login-email')]"));
-        WebElement passField = driver.findElement(By.xpath("//input[(@id='login-password')]"));
+    public void negativeLoginTest() {
+        //WebElement emailField = driver.findElement(By.xpath("//*[@id='login-email']"));
+        WebElement emailField = driver.findElement(By.id("login-email"));
+        WebElement passwordField = driver.findElement(By.id("login-password"));
         WebElement signInButton = driver.findElement(By.id("login-submit"));
 
         emailField.sendKeys("test@ukr.net");
-        passField.sendKeys("12345");
+        passwordField.sendKeys("12345");
         signInButton.click();
 
-        WebElement alertMessage = driver.findElement(By.xpath("//div[@id='global-alert-queue']//strong[not(text()=\"\")]"));
-        Assert.assertTrue(alertMessage.isDisplayed(), "Alert message is not displayed");
+        WebElement alertMessage = driver.findElement(By.xpath("//div[@id='global-alert-queue']//strong[not(text()='')]"));
+        Assert.assertTrue(alertMessage.isDisplayed(), "Alert message is not displayed.");
+
+
     }
+
+
 }
