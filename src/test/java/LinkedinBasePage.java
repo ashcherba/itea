1116779f6ccc72db146378c0e079b1acff415f03
anna <shcherba.anya@gmail.com>
2018-03-02 10:03:58 +0200

    BasePage - tutors sample

--- src/test/java/LinkedinBasePage.java
+++ src/test/java/LinkedinBasePage.java
@@ -4,25 +4,26 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-
 public class LinkedinBasePage {
     WebDriver driver;
-    public LinkedinBasePage (WebDriver driver) {
+
+    private WebElement userIcon;
+
+    public LinkedinBasePage (WebDriver driver){
         this.driver = driver;
     }
-    WebElement userIcon;
+
     private void initElements(){
         userIcon = driver.findElement(By.id("profile-nav-item"));
     }
 
     public boolean isSignedIn() {
         initElements();
-        waitUntilElementsClickable(userIcon);
+        waitUntilElementIsClickable(userIcon);
         return userIcon.isDisplayed();
     }
 
-    public String getPageTitle(){
+    public String getPageTitle() {
         return driver.getTitle();
     }
 
@@ -30,12 +31,16 @@ public class LinkedinBasePage {
         return driver.getCurrentUrl();
     }
 
-    public void waitUntilElementsClickable(WebElement WebElement){
-        waitUntilElementsClickable(WebElement, 10);
+    public void waitUntilElementIsClickable (WebElement webElement){
+        waitUntilElementIsClickable(webElement, 10);
     }
 
-    public void waitUntilElementsClickable(WebElement webElement, int timeOutInSeconds){
+    public void waitUntilElementIsClickable (WebElement webElement, int timeOutInSeconds){
         WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
         wait.until(ExpectedConditions.elementToBeClickable(webElement));
     }
+
+
+
+
 }
