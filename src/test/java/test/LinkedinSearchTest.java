b623ed86d898b046d5d0b0f7bd5dd1e34ec6a10a
anna <shcherba.anya@gmail.com>
2018-03-16 22:08:52 +0200

    data provider - updated negative test

--- src/test/java/test/LinkedinSearchTest.java
+++ src/test/java/test/LinkedinSearchTest.java
@@ -1,46 +1,29 @@
 package test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.LinkedinHomePage;
 import page.LinkedinLandingPage;
-import page.LinkedinSearchResults;
+import page.LinkedinSearchPage;
 
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
-public class LinkedinSearchTest {
-    WebDriver driver;
-    LinkedinLandingPage landingPage;
-    LinkedinHomePage homePage;
-    LinkedinSearchResults searchResults;
-
-    @BeforeMethod
-    public void beforeTest(){
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.linkedin.com/");
-        landingPage = new LinkedinLandingPage(driver);
-        landingPage.loginAs("aashcherba@bigmir.net", "qwertyQ1");
-        homePage = new LinkedinHomePage(driver);
-        searchResults = new LinkedinSearchResults(driver);
-    }
-
-    @AfterMethod
-    public void afterTest() {
-        driver.close();
-    }
+public class LinkedinSearchTest extends LinkedinBaseTest {
 
     @Test
     public void basicSearchTest(){
-        homePage.searchForElements("hr");
-        Assert.assertEquals(searchResults.results.size(), 10, "Number of results is wrong");
+        String searchTerm = "hr";
+//page Factory:
+        LinkedinLandingPage loginPage = new LinkedinLandingPage(driver);
+        LinkedinHomePage homePage = loginPage.loginAs("aashcherba@bigmir.net", "qwertyQ1");
+        LinkedinSearchPage searchPage = homePage.searchByTerm(searchTerm);
+        List<String> results = searchPage.getResults();
 
-        while(searchResults.getCardTitle()){
-            Assert.assertTrue(searchResults.cardTitle.contains("hr"),
+        Assert.assertEquals(results.size(), 10, "Number of results is wrong");
+        for(String result: results) {
+            Assert.assertTrue(result.toLowerCase().contains("hr"),
                     "Searchterm "+"'hr'"+ " not found in cart");
         }
     }
+
 }
